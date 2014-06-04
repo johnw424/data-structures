d59ad28252bd6e46acc28f6beb25687c06e06aea
Hack Reactor Students <johnw424@gmail.com>
2014-06-04 15:39:56 -0700

    Solve functional stack

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,15 +3,34 @@ var makeStack = function(){
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var size = 0;
 
   // Implement the methods below
   someInstance.push = function(value){
+    // store item in stack
+    storage[size] = value;
+    // increment size
+    size++;
   };
 
   someInstance.pop = function(){
+    // if size is greater than 0
+    if (size > 0) {
+      // decrement size
+      size--;
+      // store a copy of item at top of stack
+      var top = storage[size];
+      // sever connection between object and its property
+      delete storage[size];
+      // return that removed item
+      return top;
+    }
+
+    return 0;
   };
 
   someInstance.size = function(){
+    return size;
   };
 
   return someInstance;
