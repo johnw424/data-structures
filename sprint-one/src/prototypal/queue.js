77c3e5c16f56683c49efb2f99307119021f3470f
Hack Reactor Students <johnw424@gmail.com>
2014-06-05 11:13:16 -0700

    Implement prototypal queue (with explicit storage)

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,42 @@
 var makeQueue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  // Create object that delegates to queueMethods prototype
+  var someInstance = Object.create(queueMethods);
+  // Create an enqueue counter for our instance
+  someInstance._enqueueTally = 0;
+  // Create an dequeue counter for our instance
+  someInstance._dequeueTally = 0;
+  // Create storage object for our instance (for clarity)
+  someInstance._storage = {};
+  // Return our instance
+  return someInstance;
 };
 
-var queueMethods = {};
-
-
+var queueMethods = {
+  // Define enqueue method
+  'enqueue': function(value) {
+    // Store value into storage
+    this._storage[this._enqueueTally] = value;
+    // Increment enqueueTally
+    this._enqueueTally++;
+  },
+  // Define dequeue method
+  'dequeue': function() {
+    // Check if storage is empty
+    if (this.size() > 0) {
+      // Copy item at dequeueTally
+      var bottomItem = this._storage[this._dequeueTally];
+      // Delete key relationship to object
+      delete this._storage[this._dequeueTally];
+      // Increment dequeueTally
+      this._dequeueTally++;
+      // Return copied item
+      return bottomItem;
+    }
+    // Return nothing (since storage is empty)
+    return;
+  },
+  // Define size method
+  'size': function() {
+    return this._enqueueTally - this._dequeueTally;
+  }
+};
