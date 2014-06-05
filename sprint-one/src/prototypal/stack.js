e721e28dbd6d96526da99e4ca6552c8f275a2b2b
Hack Reactor Students <johnw424@gmail.com>
2014-06-05 10:41:31 -0700

    Implement prototypal stack without explicit storage object

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,36 @@
+// Implementing prototypal stack without explicit
+// storage object in someInstance. This is less code,
+// but may be less explicit for a developer viewing
+// this code
+
 var makeStack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  // Create object with stackMethods as prototype
+  var someInstance = Object.create(stackMethods);
+  // Size counter
+  someInstance._size = 0;
+  // Return object
+  return someInstance;
 };
 
-var stackMethods = {};
-
-
+// The stack prototype
+var stackMethods = {
+  // Method for push
+  'push': function(value) {
+    this[this._size] = value;
+    this._size++;
+  },
+  // Method for pop
+  'pop': function() {
+    if(this._size > 0) {
+      this._size--;
+      var topItem = this[this._size];
+      delete this[this._size];
+      return topItem;
+    }
+    return;
+  },
+  // Method for size
+  'size': function() {
+    return this._size;
+  }
+};
