809da4b3124b6f08a6e2316ca17cccdd97031aae
Hack Reactor Students <johnw424@gmail.com>
2014-06-04 21:24:16 -0700

    Implement functional-shared queue and stack

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,9 +1,42 @@
 var makeQueue = function(){
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  // create queue instance
+  var someInstance = {};
+  // create storage for queue instance
+  someInstance._storage = {};
+  // create enqueueTally for queue instance
+  someInstance._enqueueTally = 0;
+  // create dequeueTally for queue instance
+  someInstance._dequeueTally = 0;
+  // extend queue instance with queueMethods
+  _.extend(someInstance, queueMethods);
+  // return queue
+  return someInstance;
 };
 
-var queueMethods = {};
-
-
-
+var queueMethods = {
+  // create enqueue method to add new item to top of queue
+  'enqueue': function(value) {
+    // add value to storage with enqueueTally as key
+    this._storage[this._enqueueTally] = value;
+    // increment enqueueTally
+    this._enqueueTally++;
+  },
+  // create dequeue method to take out bottom-most item from queue
+  'dequeue': function() {
+    if ((this._enqueueTally - this._dequeueTally) > 0) {
+      // copy item at top of queue to temporary variable
+      var copiedTop = this._storage[this._dequeueTally];
+      // delete property association to item at key
+      delete this._storage[this._dequeueTally];
+      // increment dequeueTally
+      this._dequeueTally++;
+      // return copied item
+      return copiedTop;
+    }
+    return;
+  },
+  // create size method
+  'size': function() {
+    return this._enqueueTally - this._dequeueTally;
+  }
+};
--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,40 @@
 var makeStack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  // create stack instance
+  var someInstance = {};
+  // pseudo-private storage container
+  someInstance._storage = {};
+  // pseudo-private size counter
+  someInstance._size = 0;
+  // references to stackMethods for push, pop, & size
+  _.extend(someInstance, stackMethods);
+  // return stack instance
+  return someInstance;
 };
 
-var stackMethods = {};
-
-
+var stackMethods = {
+  // method for push
+  'push': function(value) {
+    // add value into storage with current size as it's key
+    this._storage[this._size] = value;
+    // increment size
+    this._size++;
+  },
+  // method for pop
+  'pop': function() {
+    if(this._size > 0) {
+      // decrement size
+      this._size--;
+      // copy item to removed
+      var topItem = this._storage[this._size];
+      // delete property association
+      delete this._storage[this._size];
+      // return copied item
+      return topItem;
+    }
+    return;
+  },
+  // method for size
+  'size': function() {
+    return this._size;
+  }
+};
