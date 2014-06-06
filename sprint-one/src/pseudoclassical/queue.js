1217b197e996f109a86d3685313299124386dd3e
Hack Reactor Students <johnw424@gmail.com>
2014-06-05 21:13:19 -0700

    Implement pseudoclassical queue

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,35 @@
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  // Create storage container
+  this._container = {};
+  // Create enqueue tally
+  this._enqueueTally = 0;
+  // Create dequeue tally
+  this._dequeueTally = 0;
 };
 
+// Method for enqueue item
+Queue.prototype.enqueue = function(value) {
+  // Create new key in container that points to value
+  this._container[this._enqueueTally] = value;
+  // Increment enqueue tally
+  this._enqueueTally++;
+};
+// Method for dequeue item
+Queue.prototype.dequeue = function() {
+  // Check if size > 0
+  if (this.size()) {
+    // Variable to point to bottom item
+    var removedItem = this._container[this._dequeueTally];
+    // Delete connection of bottom item from key
+    delete this._container[this._dequeueTally];
+    // Increment dequeue tally
+    this._dequeueTally++;
+    // Return item
+    return removedItem;
+  }
+};
 
+// Method for report size
+Queue.prototype.size = function() {
+  return this._enqueueTally - this._dequeueTally;
+};
