d5028fddb5579f2c96ac39cfb5704262e1952185
Hack Reactor Students <johnw424@gmail.com>
2014-06-05 23:50:55 -0700

    Implement set

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,25 @@
 var makeSet = function(){
   var set = Object.create(setPrototype);
-  set._storage = undefined;
+  set._storage = {};
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item){
+  // store item as both key and value
+  this._storage[item] = item;
 };
 
 setPrototype.contains = function(item){
+  // return whether it has the item via key or hasOwnProperty
+  // assumes we continue to only store strings
+  return !!(this._storage[item]);
 };
 
 setPrototype.remove = function(item){
+  // remove item from storage
+  delete this._storage[item];
 };
 
 /*
