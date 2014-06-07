e9d375daea4f33b6fde225b0381b575fd9d514c5
Hack Reactor Students <johnw424@gmail.com>
2014-06-06 20:10:43 -0700

    Implement tree

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,24 +1,44 @@
 var makeTree = function(value){
   var newTree = {};
   newTree.value = value;
-  newTree.children = undefined;
+  newTree.children = [];
+  // extend treeMethods to newTree
+  _.extend(newTree, treeMethods);
   return newTree;
 };
-
-
 
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value){
-
+  // create new tree and point parent to new tree
+  this.children.push(makeTree(value));
 };
 
 treeMethods.contains = function(target){
+  var found = false;
+  var searchNode = function(node) {
+    _.each(node.children, function(childNode) {
+      if(childNode.value === target) {
+        found = true;
+      } else {
+        //search child node for target
+        searchNode(childNode);
+      }
+    });
+  };
 
+  // check if current node contains target
+  if(this.value === target) { return true; }
+  searchNode(this);
+  return found;
 };
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
+
+var first = makeTree('adam');
+first.addChild('methuselah');
